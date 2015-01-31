0f5861f2622e0e8e04b6cfa9926513a60554bdc9
gmittal <gautam@mittal.net>
2015-01-30 17:35:41 -0800

    basic analytics installed, currently just grabs room name

--- revamped/server.js
+++ revamped/server.js
@@ -5,6 +5,11 @@
 var express = require('express');
 var app = express();
 
+var Keen = require("keen.io");
+var client = Keen.configure({
+    projectId: "54cc2e72c1e0ab7eb9e4c858",
+    writeKey: "ce3dfd99ae1a5c7d8621162eecb4a80acc5d1f15439288556d1bd829e95f9625954da38847aa95f2aa6567e16494519a3a70ae31b1976103ba95beff21899c63495a1aeb5b3badf1d8a80a8861f48956f948a94114287a77bf458c6ca94559ad96a11bb062133be59cf8b968276a0bdf"
+});
 
 var Firebase = require('firebase');
 
@@ -28,6 +33,14 @@ app.get('/', function (req, res) {
 app.get('/:party_name', function (req, res) {
 	console.log(req.param("party_name").toLowerCase());
 	var id = req.param("party_name").toLowerCase();
+
+	client.addEvent("basic", {"room_name": id}, function(err, res) {
+	    if (err) {
+	        console.log("Oh no, an error!");
+	    } else {
+	        console.log("Hooray, it got logged!");
+	    }
+	});
 
 
 
