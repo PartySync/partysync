f077f4a2bef26a23e7701f972b02e39d131d1893
gmittal <gautam@mittal.net>
2015-07-04 14:06:22 -0700

    access origin control headers implemented in node script

--- revamped/server.js
+++ revamped/server.js
@@ -69,6 +69,14 @@ app.use(function(req, res, next) {
   next();
 });
 
+// make it all work
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/', function (req, res) {
   // res.send('Hello World!');
   res.sendFile(__dirname + '/index.html');
